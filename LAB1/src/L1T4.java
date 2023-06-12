579dad9920a86d9248243d99e230daab76a70d5f
Rajin Hasnaine Khan <matiar@Rajins-MacBook-Pro.local>
2023-06-12 12:16:15 +0600

    Updated L1T4

--- LAB1/src/L1T4.java
+++ LAB1/src/L1T4.java
@@ -4,21 +4,22 @@ public class L1T4 {
 
     public static void main(String[] args) {
 
-        try (Scanner sc = new Scanner(System.in)) {
+        Scanner sc = new Scanner(System.in);
             
-            System.out.println("\nThis program will tell whether or not your entered year is a leap year.\n");
+        System.out.println("\nThis program will tell whether or not your entered year is a leap year.\n");
 
-            System.out.print("Enter year: ");
-            int year = sc.nextInt();
+        System.out.print("Enter year: ");
+        int year = sc.nextInt();
 
-            if ( (year%400==0) || ( (year%4==0) && (year%100!=0) ) ) {
+        if ( (year%400==0) || ( (year%4==0) && (year%100!=0) ) ) {
 
-                System.out.println(year+" is a leap year.\n");
-            }
-            else {
-
-                System.out.println(year+" is not a leap year.\n");
-            }
+            System.out.println(year+" is a leap year.\n");
         }
+        else {
+
+            System.out.println(year+" is not a leap year.\n");
+        }
+
+        sc.close();
     }
 }
