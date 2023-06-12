aecdb597047b60741d3b65b74b227870f4da225f
Rajin Hasnaine Khan <matiar@Rajins-MacBook-Pro.local>
2023-06-12 12:29:55 +0600

    Updated self codes Java

--- self/Calculator.java
+++ self/Calculator.java
@@ -7,65 +7,75 @@ public class Calculator {
         double result;
         byte flag = 0;
 
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("\nThis program will take two numbers from you and perform operations as told.\n");
+        Scanner sc = new Scanner(System.in);
 
-            System.out.print("Please enter the first number: ");
-            double num1 = sc.nextDouble();
+        System.out.println("\nThis program will take two numbers from you and perform operations as told.\n");
 
-            System.out.print("Please enter the second number: ");
-            double num2 = sc.nextDouble();
+        System.out.print("Please enter the first number: ");
+        double num1 = sc.nextDouble();
 
-            System.out.println("\nWhat would you like to do with these numbers?\n");
+        System.out.print("Please enter the second number: ");
+        double num2 = sc.nextDouble();
 
-            while (true) {
+        System.out.println("\nWhat would you like to do with these numbers?\n");
 
-                System.out.print("Enter 1 for addition (+),\n      2 for subtraction (-),\n      3 for multiplication (*),\n      4 for division (/),\n      5 for remainder (% or modulus)\n\n:");
-                byte operator = sc.nextByte();
+        while (true) {
 
-                System.out.println("\n");
+            System.out.print("Enter 1 for addition (+),\n      2 for subtraction (-),\n      3 for multiplication (*),\n      4 for division (/),\n      5 for remainder (% or modulus)\n\n:");
+            byte operator = sc.nextByte();
 
-                switch (operator) {
+            System.out.println("\n");
 
-                    case 1 : result = num1 + num2;
-                        System.out.println(num1 + " + " + num2 + " = " + result);
-                        flag = 1;
-                    break;
+            switch (operator) {
 
-                    case 2 : result = num1 - num2;
-                        System.out.println(num1 + " - " + num2 + " = " + result);
-                        flag = 1;
-                    break;
+                case 1 : result = num1 + num2;
+                
+                    System.out.println(num1 + " + " + num2 + " = " + result);
+                    flag = 1;
+                break;
 
-                    case 3: result = num1 * num2;
-                        System.out.println(num1 + " * " + num2 + " = " + result);
-                        flag = 1;
-                    break;
+                case 2 : result = num1 - num2;
+                
+                    System.out.println(num1 + " - " + num2 + " = " + result);
+                    flag = 1;
+                break;
 
-                    case 4: result = num1 / num2;
-                        System.out.println(num1 + " / " + num2 + " = " + result);
-                        flag = 1;
-                    break;
+                case 3: result = num1 * num2;
+                
+                    System.out.println(num1 + " * " + num2 + " = " + result);
+                    flag = 1;
+                break;
 
-                    case 5: result = num1 % num2;
-                        System.out.println(num1 + " % " + num2 + " = " + result);
-                        flag = 1;
-                    break;
+                case 4: result = num1 / num2;
+                
+                    System.out.println(num1 + " / " + num2 + " = " + result);
+                    flag = 1;
+                break;
 
-                    default: System.out.println("Please enter a valid number for an operation.");
+                case 5: result = num1 % num2;
+                
+                    System.out.println(num1 + " % " + num2 + " = " + result);
+                    flag = 1;
+                break;
 
-                }
-                if (flag==1) {
-                    break;
-                }
-                else {
-                    continue;
-                }
+                default: System.out.println("Please enter a valid number for an operation.");
 
             }
+            if (flag==1) {
+                
+                break;
+            }
+            else {
+                
+                continue;
+            }
+
         }
 
+
         System.out.println("\n");
+
+        sc.close();
     
     }
 }
